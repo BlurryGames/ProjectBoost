b95d26b3e49c9465c6f74d683c08b25a00bb0892
BlurryGames <julian.lb.games@gmail.com>
2024-05-30 17:16:52 +0200

    Section2: Project Boost / Lesson9: Collision Detection

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 class_name Player extends RigidBody3D
 
-func _process(delta: float)-> void:
+func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * 1000.0)
 	
@@ -9,3 +9,10 @@ func _process(delta: float)-> void:
 	
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print("You win!")
+	
+	if "Hazard" in body.get_groups():
+		print("You crashed!")

076180b68f0321b9cc49cead0edc54c676d8bc40
BlurryGames <julian.lb.games@gmail.com>
2024-05-30 18:38:34 +0200

    Section2: Project Boost / Lesson13: Introduciong Tweens

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ class_name Player extends RigidBody3D
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
@@ -15,16 +17,26 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		complete_level(body.file_path)
-	
-	if "Hazard" in body.get_groups():
-		crash_sequence()
+	if not is_transitioning:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+		
+		if "Hazard" in body.get_groups():
+			crash_sequence()
 
 func crash_sequence() -> void:
 	print("KABOOM!")
-	get_tree().reload_current_scene()
+	set_process(false)
+	is_transitioning = true
+	var tween: Tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete")
-	get_tree().change_scene_to_file(next_level_file)
+	set_process(false)
+	is_transitioning = true
+	var tween: Tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(
+			get_tree().change_scene_to_file.bind(next_level_file))

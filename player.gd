e1871bc6e52109ffb6d195dbbfae102928239614
BlurryGames <julian.lb.games@gmail.com>
2024-05-30 15:31:47 +0200

    Section2: Project Boost / Lesson4: Controlling the Player

--- player.gd
+++ player.gd
@@ -1,14 +1,9 @@
 class_name Player extends Node3D
 
-var timer: float = 0.0
-var growing_number: int = 42
-
-func _ready()-> void:
-	var my_number: int = 10
-	print(my_number + 1)
-
 func _process(delta: float)-> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		print("Enter was pressed!")
-		growing_number += 5
-		print(growing_number)
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
